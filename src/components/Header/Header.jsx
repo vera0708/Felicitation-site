65df2a5aaf26fd834a5d00dae84ff384c01c32c2
Vera Deeva <verasdee@gmail.com>
2023-01-29 14:01:30 +0100

    feat: add header actions

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,39 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchImage } from '../../store/imageSlice';
+import { fetchText } from '../../store/textSlice';
 import Container from '../Container/Container';
 import Choices from './Choices/Choices';
 import style from './Header.module.css';
 
-const Header = () => (
-    <header className={style.header}>
-        <Container>
-            <div className={style.wrapper}>
-                <div className={style.buttons}>
-                    <button className={style.button}>Поменять поздравление</button>
-                    <button className={style.button}>Поменять фон</button>
+
+
+const Header = () => {
+    const dispatch = useDispatch()
+    const { holiday } = useSelector(state => state.holidays)
+
+    return (
+        <header className={style.header}>
+            <Container>
+                <div className={style.wrapper}>
+                    <div className={style.buttons}>
+                        <button
+                            className={style.button}
+                            disabled={!holiday}
+                            onClick={() =>
+                                dispatch(fetchText(holiday))}
+                        >Поменять поздравление</button>
+                        <button
+                            className={style.button}
+                            onClick={() => {
+                                dispatch(fetchImage(holiday))
+                            }}
+                        >Поменять фон</button>
+                    </div>
+                    <Choices />
                 </div>
-                <Choices />
-            </div>
-        </Container>
-    </header>
-)
+            </Container>
+        </header>
+    )
+}
 
 export default Header
